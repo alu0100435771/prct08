0bdf7f3850214a65d4554a8c27d897cbb1993a08
Sergio Oramas <alu0100603231@ull.edu.es>
2013-11-05 19:36:14 +0000

    Corrigiendo errores de los test

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,34 +1,34 @@
 require 'rspec'
-require './Matriz.rb'
+require './lib/Matriz.rb'
 
 describe Matriz do
 
 	  before :each do
-		@a = Matriz.new(2,2[[2,4],[5,3]])
-		@b = Matriz.new(3,3[[3,5,6],[2,4,7],[4,6,3]])
-		@c = Matriz.new(2,3[[4,6,4],[2,3,3]])
-		@d = Matriz.new(3,2[[2,3],[3,2],[2,2]])
-		@e = Matriz.new(2,2[[4,4],[3,3]])
+		@a = Matriz.new(2,2,[[2,4],[5,3]])
+		@b = Matriz.new(3,3,[[3,5,6],[2,4,7],[4,6,3]])
+		@c = Matriz.new(2,3,[[4,6,4],[2,3,3]])
+		@d = Matriz.new(3,2,[[2,3],[3,2],[2,2]])
+		@e = Matriz.new(2,2,[[4,4],[3,3]])
 
 	  end
 
     # Comprobacion de que se crea bien la matriz
         describe "Comprobamos el numero de filas y columnas" do
-                it " - Filas para una matriz 3x3\n" do
-                        @a.fil.should eq(3)
+                it " - Filas para una matriz 2x2\n" do
+                        @a.fil.should eq(2)
                 end
-                it " - Columnas de una matriz 3x3\n" do
-                        @a.col.should eq(3)
+                it " - Columnas de una matriz 2x2\n" do
+                        @a.col.should eq(2)
                 end
         end
         
         describe "Comprobacion del metodo de conversion a string" do
                 it "\n" do
-                        @a.to_s.should eq("1 2 \n3 4 \n")
+                        @a.to_s.should eq("2 4 \n5 3 \n")
                 end
 
                 it "Acceso al elemento [1][1]" do
-                        @a.matriz[0][0].shoul ed(4)
+                        @a.matriz[1][1].should eq(3)
                 end
 
         end
@@ -59,5 +59,7 @@ describe Matriz do
                         @temp2 = @d.mult(2)
                         @temp2.to_s.should eq(@temp.to_s)
                 end
-        end
-        
+
+	end
+end
+
