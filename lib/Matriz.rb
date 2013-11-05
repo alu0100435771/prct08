981ce4e5c15f77dc13b997c1d20dd9957f9ef160
Yeray Hernandez <alu0100435771@ull.edu.es>
2013-11-05 18:40:41 +0000

    Añadiendo metodo suma (+)

--- lib/Matriz.rb
+++ lib/Matriz.rb
@@ -30,4 +30,16 @@ class Matriz
     temp
   end
 
+  def +(other) # Metodo que suma 2 matrices
+    raise ArgumentError, 'Las matrices no tienen el mismo tamaño, no se pueden sumar' unless ((@fil == other.fil) || (@col == other.col))
+    mat = Array.new(0)
+      for i in 0...filas do
+        fila = Array.new(0)
+        for j in 0...colum do
+          fila << matriz[i][j] + other.matriz[i][j]
+        end
+        mat << fila
+      end
+    Matriz.new(@fil, @col, mat)
+  end
 end
